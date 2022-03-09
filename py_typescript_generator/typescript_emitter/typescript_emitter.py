65a9f1e804e14b2581a091fae4e4b73a66321e89
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-03-09 19:44:56 +0100

    feat: emit Typescript to String

---
+++ py_typescript_generator/typescript_emitter/typescript_emitter.py
@@ -0,0 +1,40 @@
+from py_typescript_generator.typescript_model_compiler.ts_model import TsModel
+from py_typescript_generator.typescript_model_compiler.ts_object_type import (
+    TsObjectType,
+)
+
+
+class TypescriptEmitter:
+    def emit(self, ts_model: TsModel) -> str:
+        typescript_str = ""
+        for ts_type in ts_model.types:
+            typescript_str += self._emit_type(ts_type)
+        for ts_enum in ts_model.enums:
+            typescript_str += self._emit_enum(ts_enum)
+        return typescript_str
+
+    def _emit_enum(self, ts_enum):
+        enum_template = "enum "
+        enum_template += ts_enum.name
+        enum_template += " {\n"
+
+        for value in ts_enum.values:
+            enum_template += f"    {value.name} = {value.format_value()},\n"
+
+        enum_template += "}\n"
+
+        return enum_template
+
+    def _emit_type(self, ts_type: TsObjectType) -> str:
+        type_template = "interface "
+        type_template += ts_type.name
+        type_template += " {\n"
+
+        for field in ts_type.fields:
+            type_template += (
+                f"    {field.name}: {field.type.format_as_type_reference()}\n"
+            )
+
+        type_template += "}\n"
+
+        return type_template
---
+++ tests/unittests/typescript_emitter/test_typescript_emitter.py
@@ -0,0 +1,116 @@
+from py_typescript_generator.typescript_emitter.typescript_emitter import (
+    TypescriptEmitter,
+)
+from py_typescript_generator.typescript_model_compiler.ts_enum import TsEnum
+from py_typescript_generator.typescript_model_compiler.ts_model import TsModel
+from py_typescript_generator.typescript_model_compiler.ts_object_type import (
+    TsObjectType,
+)
+from tests.unittests.fixture_classes import ClassFixture, EnumFixture
+
+
+def _emit_object(ts_object_type: TsObjectType) -> str:
+    ts_model = TsModel.of_object_types([ts_object_type])
+    return TypescriptEmitter().emit(ts_model)
+
+
+def _emit_enum(ts_enum: TsEnum) -> str:
+    ts_model = TsModel.of_enums([ts_enum])
+    return TypescriptEmitter().emit(ts_model)
+
+
+def test_emit_empty_class(empty_class: ClassFixture) -> None:
+    assert (
+        _emit_object(empty_class.ts_object_type)
+        == """interface EmptyClass {
+}
+"""
+    )
+
+
+def test_emit_class_with_class_with_empty_class(
+    class_with_class_with_empty_class: ClassFixture,
+) -> None:
+    assert (
+        _emit_object(class_with_class_with_empty_class.ts_object_type)
+        == """interface ClassWithClassWithEmptyClass {
+    class_with_empty_class: ClassWithEmptyClass
+}
+"""
+    )
+
+
+def test_emit_class_with_str_list(class_with_str_list: ClassFixture) -> None:
+    assert (
+        _emit_object(class_with_str_list.ts_object_type)
+        == """interface ClassWithStrList {
+    str_list: string[]
+}
+"""
+    )
+
+
+def test_emit_class_with_str_str_dict(class_with_str_str_dict: ClassFixture) -> None:
+    assert (
+        _emit_object(class_with_str_str_dict.ts_object_type)
+        == """interface ClassWithStrStrDict {
+    str_dict: {[index: string]: string}
+}
+"""
+    )
+
+
+def test_emit_class_with_empty_class_list(
+    class_with_empty_class_list: ClassFixture,
+) -> None:
+    assert (
+        _emit_object(class_with_empty_class_list.ts_object_type)
+        == """interface ClassWithEmptyClassList {
+    empty_class_list: EmptyClass[]
+}
+"""
+    )
+
+
+def test_emit_class_with_optional_int(class_with_optional_int: ClassFixture) -> None:
+    assert (
+        _emit_object(class_with_optional_int.ts_object_type)
+        == """interface ClassWithOptionalInt {
+    value: number | undefined
+}
+"""
+    )
+
+
+def test_emit_class_with_optional_empty_class(
+    class_with_optional_empty_class: ClassFixture,
+) -> None:
+    assert (
+        _emit_object(class_with_optional_empty_class.ts_object_type)
+        == """interface ClassWithOptionalEmptyClass {
+    value: EmptyClass | undefined
+}
+"""
+    )
+
+
+def test_emit_simple_int_enum(simple_int_enum: EnumFixture) -> None:
+    assert (
+        _emit_enum(simple_int_enum.ts_enum)
+        == """enum SimpleIntEnum {
+    SECOND = 1,
+    FIRST = 0,
+}
+"""
+    )
+
+
+def test_emit_simple_str_enum(simple_str_enum: EnumFixture) -> None:
+    assert (
+        _emit_enum(simple_str_enum.ts_enum)
+        == """interface ClassWithClassWithEmptyClass {
+    SECOND = "SECOND",
+    FIRST = "FIRST",
+}
+"""
+    )

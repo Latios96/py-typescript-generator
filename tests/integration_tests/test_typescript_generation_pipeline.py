85f09c4620bbde1dd153ebc2d50f15bb43f4d2b3
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-09-29 15:20:41 +0200

    test: add tagged union integ test

--- tests/integration_tests/test_typescript_generation_pipeline.py
+++ tests/integration_tests/test_typescript_generation_pipeline.py
@@ -10,10 +10,20 @@ def test_build_pipeline(tmp_path):
     class MyExampleClass:
         value: int
 
+    @dataclass
+    class TaggedUnionRoot:
+        __json_type_info_attribute__ = "type"
+
+    @dataclass
+    class TaggedUnionChild(TaggedUnionRoot):
+        type = "CHILD"
+
     output_file = tmp_path / "some_folder" / "test.ts"
-    TypeGenerationPipelineBuilder().for_types([MyExampleClass]).with_type_overrides(
-        {int: str}
-    ).convert_field_names_to_camel_case().to_file(output_file).build().run()
+    TypeGenerationPipelineBuilder().for_types(
+        [MyExampleClass, TaggedUnionRoot]
+    ).with_type_overrides({int: str}).convert_field_names_to_camel_case().to_file(
+        output_file
+    ).build().run()
 
     with open(output_file, "r") as f:
         content = f.read()
@@ -23,5 +33,9 @@ def test_build_pipeline(tmp_path):
         == """export interface MyExampleClass {
     value: string
 }
+export interface TaggedUnionChild {
+    type: "CHILD"
+}
+export type TaggedUnionRoot = TaggedUnionChild;
 """
     )
